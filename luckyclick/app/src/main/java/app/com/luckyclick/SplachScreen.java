8a0cb1294732fcc988ab3eeef638f7758285c712
Frad Ali <alifradi2011@gmail.com>
2017-08-20 14:43:52 +0100

    first commit

---
+++ luckyclick/app/src/main/java/app/com/luckyclick/SplachScreen.java
@@ -0,0 +1,13 @@
+package app.com.luckyclick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplachScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach_screen);
+    }
+}
